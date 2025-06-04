0c745753bb8056fd00c286ba549d840a8feb2f1b
RenukaGaidhane <154632511+RenukaGaidhane@users.noreply.github.com>
2025-06-04 17:28:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+
 const server = http.createServer((req, res) => {
   res.end('Hello from Jenkins-hosted app!');
 });
 
-server.listen(3006, () => {
-  console.log('Server running on http://localhost:3006');
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
