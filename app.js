f777122036f765a713c702577ec8bff117488e6b
Harsh Meshram <harshmeshram0099@gmail.com>
2025-06-04 17:22:30 +0530

    change port from 3000 to 3006

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Jenkins-hosted app!');
 });
 
-server.listen(3000, () => {
+server.listen(3006, () => {
   console.log('Server running on http://localhost:3000');
 });
